d392fa02b950f25b74209fe9f3b61bb66e2b787f
David Cramer <dcramer@gmail.com>
2011-06-03 14:36:10 -0700

    Initial working details pane

--- nexus_redis/nexus_modules.py
+++ nexus_redis/nexus_modules.py
@@ -1,8 +1,6 @@
 import socket
 from redis import Redis
 
-from django.utils.datastructures import SortedDict
-
 import nexus
 
 from nexus_redis import conf
@@ -26,19 +24,21 @@ class RedisModule(nexus.NexusModule):
         default_timeout = socket.getdefaulttimeout()
         socket.setdefaulttimeout(timeout)
 
+        results = {}
         for group, hosts in self.get_caches().iteritems():
-            res = []
-            for host in hosts:
+            results[group] = []
+            for host, conn in hosts:
                 try:
-                    stats = host.info()
-                except:
+                    stats = conn.info()
+                except Exception, e:
                     stats = {'online': 0}
                 else:
                     stats['online'] = 1
-                res.append((host, stats))
-            yield res
+                results[group].append((host, stats))
 
         socket.setdefaulttimeout(default_timeout)
+
+        return results
     
     def get_title(self):
         return 'Redis'
@@ -53,37 +53,31 @@ class RedisModule(nexus.NexusModule):
         return urlpatterns
     
     def render_on_dashboard(self, request):
-        cache_stats = list(self.get_stats())
-        
+        cache_stats = self.get_stats()
+         
         global_stats = {
-            'bytes': 0,
-            'limit_maxbytes': 0,
-            'curr_items': 0,
-            'curr_connections': 0,
-            'total_connections': 0,
-            'total_items': 0,
-            'cmd_get': 0,
-            'get_hits': 0,
-            'get_misses': 0,
-            'rusage_system': 0,
+            'expired_keys': 0,
+            'total_commands_processed': 0,
+            'used_memory': 0,
+            'connected_clients': 0,
+            'connected_slaves': 0,
             'online': 0,
         }
-        for host, stats in cache_stats:
-            for k in global_stats.iterkeys():
-                global_stats[k] += float(stats.get(k, 0))
-        global_stats['total'] = len(cache_stats)
+
+        servers = 0
+        for group in cache_stats.itervalues():
+            for host, stats in group:
+                for k in global_stats.iterkeys():
+                    global_stats[k] += float(stats.get(k, 0))
+                    servers += 1
+        global_stats['total'] = servers
 
         return self.render_to_string('nexus/redis/dashboard.html', {
             'global_stats': global_stats,
         })
     
     def index(self, request):
-        try:
-            cache_stats = ((k, SortedDict(sorted(v.iteritems(), key=lambda x: x[0]))) for k, v in self.get_stats())
-        except AttributeError:
-            cache_stats = []
-        
         return self.render_to_response("nexus/redis/index.html", {
-            'cache_stats': cache_stats,
+            'cache_stats': self.get_stats(),
         }, request)
 nexus.site.register(RedisModule, 'redis', category='cache')
